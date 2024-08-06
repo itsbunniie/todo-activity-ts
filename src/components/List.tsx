7b1893a8d68594f4a0b1bf0d4ae7f32002560bda
Ivy Gozarin <iggozarin@gmail.com>
2024-08-06 12:08:24 +0800

    Update List.tsx

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,16 +1,24 @@
 import Item from "./Item";
+import { useContext } from "react";
+import { todoContext } from "../providers/todoContext";
 
 const List = () => {
+  const { todos } = useContext(todoContext);
+
   return (
     <div className="list-container">
+      {/* list of uncompleted todos */}
       <div className="list-header">
         Your Tasks <div className="counter-list">0</div>
       </div>
-      <Item />
+      {todos.map((todo) => (
+        <Item key={todo.id} {...todo} />
+      ))}
+
+      {/* list of uncompleted todos */}
       <div className="list-header">
         Completed Tasks <div className="counter-list">0</div>
       </div>
-      <div className="empty-list-text">No tasks here!</div>
     </div>
   );
 };
